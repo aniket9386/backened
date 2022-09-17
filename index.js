336e8753b42a88b754d9d49afff423045af81ab5
Aniket <aniketgupta7372@gmail.com>
2022-09-17 19:57:56 +0530

    doneb

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const connectToMongo = require('./db');
-const express = require('express')
+const express = require('express');
+var cors = require('cors');
+
 connectToMongo();
-const app = express()
-const port = 5000
+const app = express();
+const port = 5000;
+
+app.use(cors())
+
 
 app.use(express.json())
 
@@ -12,5 +17,5 @@ app.use('/api/notes', require('./routes/notes'))
 
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`iNoteBook listening at http://localhost:${port}`)
 })
--- routes/auth.js
+++ routes/auth.js
@@ -8,12 +8,14 @@ var fetchuser = require('../middleware/fetchUser');
 
 const JWT_SECRET = 'akcoding';
 
+
 // ROUTE 1 : Create user using post "/api/auth/createuser" (authentication not required)
 router.post('/createuser', [
     body('name', 'Enter a valid name').isLength({ min: 3 }),
     body('email', 'Enter a valid email').isEmail(),
     body('password', 'Password must be atleast 5 characters').isLength({ min: 5 }),
 ], async (req, res) => {
+    let success = false;
 
     // if error return bad request
     const errors = validationResult(req);
@@ -26,7 +28,7 @@ router.post('/createuser', [
     try {
         let user = await User.findOne({ email: req.body.email });
         if (user) {
-            return res.status(400).json({ error: "Sorry a user with this email already exits" })
+            return res.status(400).json({ success, error: "Sorry a user with this email already exits" })
         }
         const salt = await bcrypt.genSalt(10);
         const secPass = await bcrypt.hash(req.body.password, salt);
@@ -44,7 +46,8 @@ router.post('/createuser', [
         const authtoken = jwt.sign(data, JWT_SECRET)
 
         // res.json(user)
-        res.json({ authtoken })
+        success = true;
+        res.json({ success, authtoken })
 
     } catch (error) {
         console.error(error.message);
@@ -58,20 +61,21 @@ router.post('/login', [
     body('email', 'Enter a valid email').isEmail(),
     body('password', 'Password cannot be blank').exists(),
 ], async (req, res) => {
+    let success = false;
     // if error return bad request
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+        return res.status(400).json({ success, errors: errors.array() });
     }
     const { email, password } = req.body;
     try {
         let user = await User.findOne({ email });
         if (!user) {
-            return res.status(400).json({ errors: "Please try to login with correct credientials" });
+            return res.status(400).json({ success, errors: "Please try to login with correct credientials" });
         }
         const passwordCompare = await bcrypt.compare(password, user.password);
         if (!passwordCompare) {
-            return res.status(400).json({ errors: "Please try to login with correct credientials" });
+            return res.status(400).json({ success, errors: "Please try to login with correct credientials" });
         }
         const data = {
             user: {
@@ -81,7 +85,8 @@ router.post('/login', [
         const authtoken = jwt.sign(data, JWT_SECRET)
 
         // res.json(user)
-        res.json({ authtoken })
+        success = true;
+        res.json({ success, authtoken })
 
 
     } catch (error) {
