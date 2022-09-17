f62458017e94b50962942206aa8719167426959e
Aniket <aniketgupta7372@gmail.com>
2022-09-17 20:51:35 +0530

    mongo cloud added

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017/inotebook"
+// const mongoURI = "mongodb://localhost:27017/inotebook"
+const mongoURI = "mongodb+srv://aniket9386:aniket123@cluster0.cesuuvx.mongodb.net/?retryWrites=true&w=majority"
 const connectToMongo = () => {
     mongoose.connect(mongoURI, () => {
         console.log("Connected to Mongo Successfully");
